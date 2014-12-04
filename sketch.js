9cb5102032498b50b3d405ff122b7694c86fb637
jmjvvv <jmjvvv@gmail.com>
2014-12-04 17:14:29 +0900

    f

--- sketch.js
+++ sketch.js
@@ -6,12 +6,19 @@ function setup() {
   // into the size() method. In this program, the size of the image
   // is 710x400 pixls.
   bg = loadImage("p.jpg");
+  img=loadImage("a.png");
   createCanvas(720, 400);
 }
 
 function draw() {
+   var duration1 = 5000;
+   var timing1 = (new Date()%duration1)/duration1;
   background(bg);
-  
+
+  image(img,600 + Math.cos(timing1*2*PI)*500,          
+        400 + Math.sin(timing1*2*PI)*0);
+
+
   stroke(226, 204, 0);
   line(0, y, width, y);
   
