a66de3b3f4c4687d091b8d362d633d9b67359550
jmjvvv <jmjvvv@gmail.com>
2014-12-07 16:06:39 +0900

    d

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 
  var bugs = []; // array of Jitter objects
  var img;
- var img2;
+
 
 
 
@@ -9,7 +9,7 @@ function setup() {
   createCanvas(710, 400);
   // Create objects
  img = loadImage("p.jpg");
- img2=loadImage("a.png");
+
 
   for (var i=0; i<50; i++) {
     bugs.push(new Jitter());
@@ -26,13 +26,7 @@ function draw() {
    
   }
 }
-drawImg();
-function drawImg(){
-   var duration1 = 5000;
-    var timing1 = (new Date()%duration1)/duration1;
-    image(img2,600 + Math.cos(timing1*2*PI)*500,          
-        400 + Math.sin(timing1*2*PI)*0);
-}
+
 
 // Jitter class
 function Jitter() {
