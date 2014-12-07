e9e2dfa66accf5a1affa0518fb99470259cb4c28
jmjvvv <jmjvvv@gmail.com>
2014-12-07 16:47:04 +0900

    d

--- sketch.js
+++ sketch.js
@@ -2,16 +2,20 @@
  var bugs = []; // array of Jitter objects
  var img;
 
+ var diameter; 
+var angle = 0;
+
 
 
 
 
  function setup() {
   createCanvas(710, 400);
+  diameter = height - 10;
+  noStroke();
+  fill(255, 204, 0);
   // Create objects
   img = loadImage("p.jpg");
- 
-
   for (var i=0; i<50; i++) {
     bugs.push(new Jitter());
   }
@@ -20,6 +24,17 @@
 function draw() {
 
   background(img);
+  var d1 = 10 + (sin(angle) * diameter/2) + diameter/2;
+  var d2 = 10 + (sin(angle + PI/2) * diameter/2) + diameter/2;
+  var d3 = 10 + (sin(angle + PI) * diameter/2) + diameter/2;
+  
+  ellipse(0, height/2, d1, d1);
+  ellipse(width/2, height/2, d2, d2);
+  ellipse(width, height/2, d3, d3);
+  
+  angle += 0.02;
+
+  
   for (var i=0; i<bugs.length; i++) {
     bugs[i].move();
     bugs[i].display();
