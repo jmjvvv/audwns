e1349f84190521f6b1ca97e1f187c925d5a9f3fe
jmjvvv <jmjvvv@gmail.com>
2014-12-07 17:11:01 +0900

    d

--- sketch.js
+++ sketch.js
@@ -26,11 +26,11 @@ function draw() {
   background(img);
   var d1 = 1 + (sin(angle + PI/20) * diameter/20) + diameter/20;
   var d2 = 1 + (sin(angle + PI/20) * diameter/20) + diameter/20;
-
+var d3 = 10 + (sin(angle + PI) * diameter/2) + diameter/2;
   
   rect(0, height/3, d1, d1);
   rect(width/2, height/2, d2, d2);
-
+ellipse(width, height/2, d3, d3);
   
   angle += 0.02;
 
